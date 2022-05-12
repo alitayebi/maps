cc6b0e0745991928cae92d02acf785c35220f807
Ali Tayebi <42505961+alitayebi@users.noreply.github.com>
2022-05-12 06:21:22 +0430

    update rent map

--- rent/js/main.js
+++ rent/js/main.js
@@ -1,254 +1,130 @@
-	$(document).ready(function() {
-
-		var region;
-		var payValue = 4;
-
-		var map = L.map('map', {
-			touchZoom: false,
-			doubleClickZoom: false,
-			scrollWheelZoom: false,
-			boxZoom: false,
-			dragging: false,
+var region, timestampN,inflData,regionsData,
+		request = {timestamp: '', timestamps: '', timeTxt: '', salValue: 10, salCalbr: '', salShare: 30, aptMeter: 60, rent: '', rentMil:''},
+		failNote = "در فراخوانی داده‌ها به مشکلی برخوردیم. صفحه را مجدداً بارگذاری کنید."
+		map = L.map('map', {
 			keyboard: false,
 		});
+L.tileLayer('https://tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=48584cb50e384399b635995577038af7', {
+		maxZoom: 20,
+		attribution: '&copy; Ali Tayebi | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Thunderforest'
+	}).addTo(map);
+$.ajax({
+  url:"data/infl.json",
+  dataType: 'json',
+  async: false,
+  success: function(data) {
+    inflData = data;
+  }
+});
+$.ajax({
+  url:"data/regions8800.geojson",
+  dataType: 'json',
+  async: false,
+  success: function(data) {
+    geoData = data;
+  }
+});
+if (inflData.infl.length == (Object.keys(geoData.features[0].properties).length -1)) {
+	region = L.geoJson(geoData, {
+					style: { color: "#000", fillOpacity: .65}
+	}).addTo(map);
+	map.fitBounds(region.getBounds(), {padding: [-100, -100]});
+			$(".leaflet-control-zoom").css("visibility", "hidden");
+	request["timestamps"] = inflData.infl.length
+	request["timestamp"] = inflData.infl.length;
+	timestamp = request["timestamp"];
+	request["timeTxt"] = inflData.infl[timestamp-1].Time;
+	updateSymbology()
+} else {
+	alert(failNote);
+}
 
-		var text = '{"infl":[' +
-		'{"Time":"بهار ۱۳۹۶","Inflation":"0.68708" },' +
-		'{"Time":"تابستان ۱۳۹۶","Inflation":"0.68708" },' +
-		'{"Time":"پائیز ۱۳۹۶","Inflation":"0.68708" },' +
-		'{"Time":"زمستان ۱۳۹۶","Inflation":"0.68708" },' +
-		'{"Time":"بهار ۱۳۹۷","Inflation":"0.76953" },' +
-		'{"Time":"تابستان ۱۳۹۷","Inflation":"0.76953" },' +
-		'{"Time":"پائیز ۱۳۹۷","Inflation":"0.76953" },' +
-		'{"Time":"زمستان ۱۳۹۷","Inflation":"0.76953" },' +
-		'{"Time":"بهار ۱۳۹۸","Inflation":"0.86957" },' +
-		'{"Time":"تابستان ۱۳۹۸","Inflation":"0.86957" },' +
-		'{"Time":"پائیز ۱۳۹۸","Inflation":"0.86957" },' +
-		'{"Time":"زمستان ۱۳۹۸","Inflation":"0.86957" },' +
-		'{"Time":"بهار ۱۳۹۹","Inflation":"1" }]}';
 
-		L.tileLayer('https://tile.thunderforest.com/transport/{z}/{x}/{y}.png?apikey=48584cb50e384399b635995577038af7', {
-        	maxZoom: 20,
-        	attribution: '&copy; Ali Tayebi | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Thunderforest'
-        }).addTo(map);
-	$.getJSON("data/regionsR60pg96-99.geojson")
-		.done(function(data) {
-			var info = processData(data);
-			createPropSymbols(info.timestamps, data);
-			createSliderUI(info.timestamps);
-	 	})
-	.fail(function() { alert("There has been a problem loading the data.")});
-	function processData(data) {
-		var timestamps = [];
-		var min = Infinity;
-		var max = -Infinity;
-
-		for (var feature in data.features) {
-
-			var properties = data.features[feature].properties;
-
-			for (var attribute in properties) {
-
-				if (  attribute != 'region' ) {
-
-					if ( $.inArray(attribute,timestamps) === -1) {
-						timestamps.push(attribute);
-					}
-
-					if (properties[attribute] < min) {
-						min = properties[attribute];
-					}
-
-					if (properties[attribute] > max) {
-						max = properties[attribute];
-					}
-				}
+function updateSymbology() {
+	timestamp = request["timestamp"];
+	request["timeTxt"] = inflData.infl[timestamp-1].Time;
+	if (request["salValue"] <= 2.6) {
+		var inflRate = inflData.infl[timestamp-1].Rate1;
+	} else {
+		var inflRate = inflData.infl[timestamp-1].Rate2;
+	}
+	request["rent"]=request["salValue"]*1000000*inflRate * (request["salShare"]/100);
+	request["salCalbr"]= (Math.round((request["salValue"]*inflRate)*10))/ 10
+	request["rentMil"]= (Math.round((request["salValue"]*inflRate * (request["salShare"]/100))*10))/ 10
+	for (var key in request) {
+		$('.'+key).text(request[key]);
+	}
+	region.eachLayer(function(layer) {
+		if (layer.feature.properties[timestamp] == '-') {
+			var fillColor = "red"
+		} else {
+			if ((layer.feature.properties[timestamp]/10)*request["aptMeter"] < request["rent"]) {
+				var fillColor = "yellow";
+			} else {
+				var fillColor = "black";
 			}
 		}
-
-		return {
-			timestamps : timestamps,
-			min : min,
-			max : max
-		}
-	}
-
-	function createPropSymbols(timestamps, data) {
-
-		region = L.geoJson(data, {
-            style: { color: "#000", fillOpacity: .65}
-
-		}).addTo(map);
-		map.fitBounds(region.getBounds(), {padding: [-50, -50]});
-        $(".leaflet-control-zoom").css("visibility", "hidden");
-
-		updatePropSymbols(timestamps[12], payValue);
-	}
-	function updatePropSymbols(timestamp, payValue) {
-	    obj = JSON.parse(text);
-        rateValue =  obj.infl[timestamp-1].Inflation;
-	    payCalb =(Math.round(payValue*rateValue*10))/ 10;
-	    if (timestamp < 13) {
-	        	    $(".temporal-payCalb").text("("+payCalb+" مییلیون تومان)");
-
-	    } else {
-	        $(".temporal-payCalb").text("");
-	    }
-		region.eachLayer(function(layer) {
-			var props = layer.feature.properties;
-			var radius = calcPropRadius(props[timestamp], payValue);
-			var popupContent =       "<h3>منطقه " + props.region +"</h3>"+
-			        "<h4>"+ String(Math.round(props[timestamp]/60))+" تومان</h4>" +
-			        "<b>میانگین قیمت اجاره (هر متر مربع) در" +
-			        "<h4><span class='highlight temporal-legend'>بهار ۱۳۹۸</span> </h4>";
-
-			layer.setStyle({fillColor: radius});
-			layer.bindPopup(popupContent);
-		});
-	}
-	function calcPropRadius(attributeValue, payValue) {
-	    if (attributeValue < (payValue*1000000*rateValue / 2)) {
-          return "yellow";
-        } else {
-    		return "black";
-        }
-
-	}
-
-
-	function createSliderUI(timestamps) {
-	    var footNotes = L.control({ position: 'bottomleft'} );
-	    footNotes.onAdd = function(map) {
-	        var notes = L.DomUtil.create("span", "notes");
-    		$(notes).append("* داده‌های اجاره‌بها: مرکز آمار ایران <br>** نرخ رسمی افزایش حقوق سالیانه: مصوبات هیئت دولت ایران");
-	    return notes;
-	    };
-	    footNotes.addTo(map) ;
-		var sliderControl = L.control({ position: 'bottomleft'} );
-
-		sliderControl.onAdd = function(map) {
-
-			var sliderC = L.DomUtil.create("input", "range-slider");
-
-			L.DomEvent.addListener(sliderC, 'mousedown', function(e) {
-				L.DomEvent.stopPropagation(e);
-			});
-            L.DomEvent.addListener(sliderC, 'mouseover', function(e) {
-		  		map.dragging.disable();
-			});
-            L.DomEvent.addListener(sliderC, 'mouseout', function(e) {
-		  		map.dragging.enable();
-			});
-			$(sliderC)
-				.attr({'type':'range',
-					'max': timestamps[timestamps.length-1],
-					'min': timestamps[0],
-					'step': 1,
-					'orient':'horizental',
-				    'value': String(timestamps[12])
-				})
-		  		.on('input change', function() {
-		  		updatePropSymbols($(this).val().toString(), payValue);
-		  		    $(".temporal-legend").text(obj.infl[this.value-1].Time);
-		  		    $(".temporal-legendc").text(this.value);
-
-		  	});
-
-			return sliderC;
-
-		};
-		sliderControl.addTo(map) ;
-
-		createTemporalLegend(timestamps[12]);
-
-		function createTemporalLegend(startTimestamp) {
-
-		var temporalLegend = L.control({ position: 'bottomleft' });
-		var temporalLegendC = L.control({ position: 'bottomleft' });
-
-		temporalLegend.onAdd = function(map) {
-			var outpute = L.DomUtil.create("div", "temporal");
-		$(outpute).append("<span class='temporal-legend'>بهار ۱۳۹۸</span>");
-						return outpute;
-
-		}
-		temporalLegendC.onAdd = function(map) {
-			var output = L.DomUtil.create("output", "temporal-legendc");
- 			$(output).text(startTimestamp)
-			return output;
-		}
-        		temporalLegend.addTo(map);
-        		temporalLegendC.addTo(map);
-
-	}
-		var sliderControlT = L.control({ position: 'bottomleft'} );
-
-		sliderControlT.onAdd = function(map) {
-
-			var sliderT = L.DomUtil.create("input", "range-slider");
-
-			L.DomEvent.addListener(sliderT, 'mousedown', function(e) {
-				L.DomEvent.stopPropagation(e);
-			});
-            L.DomEvent.addListener(sliderT, 'mouseover', function(e) {
-		  		map.dragging.disable();
-			});
-            L.DomEvent.addListener(sliderT, 'mouseout', function(e) {
-		  		map.dragging.enable();
-			});
-			$(sliderT)
-				.attr({'type':'range',
-					'max':10,
-					'min': 1,
-					'step': 1,
-					'orient':'vertical',
-				    'value': 5
-				})
-		  		.on('input change', function() {
-		  		    $(".temporal-legendt").text(this.value);
-		  		    $(payValue = this.value);
-		  		updatePropSymbols($(".temporal-legendc").val(), $(this).val().toString());
-		  		// alert($(slider.value));
-
-
-		  	});
-
-			return sliderT;
-		};
-		sliderControlT.addTo(map) ;
-
-				createTemporalLegendT(timestamps[12]);
-
-
-	function createTemporalLegendT(startTimestamp) {
-
-		var temporalLegendT = L.control({ position: 'bottomleft' });
-
-		temporalLegendT.onAdd = function(map) {
-			var outpute = L.DomUtil.create("div", "temporal");
-			$(outpute).append("<h2 id='legendTitle'>توان اجاره‌نشینی</h2>");
-    		$(outpute).append("<p class='legendText'>در <span class='highlight temporal-legend'> بهار ۱۳۹۹</span>، شما با معادل حقوق <span class='highlight temporal-legendt'>4</span> میلیون تومان در سال ۱۳۹۸   <span class='highlight temporal-payCalb'></span> می‌توانستید با صرف پنجاه درصد از حقوق خود آپارتمانی با مساحت ۶۰ متر را در مناطق زرد رنگ اجاره کنید ");
-    		$(outpute).append("حقوق: <span class='temporal-legendt'>5</span> میلیون تومان");
-
-						return outpute;
-
-		}
-
-        		temporalLegendT.addTo(map);
-
-	}
-
-
-
+	layer.setStyle({fillColor: fillColor});
+	})
 }
-var elem = document.querySelector('input[type="range"]');
+$(document).ready(function () {
+		var legendHtml = `
+		<h2 id='legendTitle'>توان اجاره‌نشینی</h2>
+		<p>حقوق <small>(در سال ۱۴۰۰)</small>: <span class='salValue'>${request['salValue']}</span> میلیون تومان</p>
+		<input name='salValue' type="range" min="2" max="20" step="1" value="${request['salValue']}">
+		<p>سهم اجاره از حقوق: <span class='salShare'>${request['salShare']}</span> درصد</p>
+		<input name='salShare' type="range" min="10" max="100" step="10" value="${request['salShare']}">
+		<p>آپارتمان: <span class='aptMeter'>${request['aptMeter']}</span> مترمربعی</p>
+		<input name='aptMeter' type="range" min="10" max="150" step="10" value="${request['aptMeter']}">
+		<h3>تغییر در زمان:</h3>
+		<p>زمان: <span class='timeTxt'>${request['timeTxt']}</span></p>
+		<input name='timestamp' type="range" min="1" max="${request['timestamps']}" step="1" value="${request['timestamps']}">
+		<small>معادل حقوق: <span class="salCalbr">${request['salCalbr']}</span> | معادل اجاره: <span class="rentMil">${request['rentMil']}</span> (میلیون تومان)
+		<p class='legendText'>در <span class='timeTxt highlight'> ${request['timeTxt']}</span>
+		، شما با <span class='salShare highlight'>${request['salShare']}</span> درصد معادل حقوق خود
+		 می‌توانستید آپارتمانی با مساحت
+		 <span class='aptMeter highlight'>${request['aptMeter']}</span> مترمربع را در مناطق زرد رنگ اجاره کنید</p>
+		<p class="notes">* داده‌های اجاره‌بها: مرکز آمار ایران <br>** نرخ رسمی افزایش حقوق سالیانه: مصوبات هیئت دولت ایران</p>
+		`;
+		$('.legend-content').append(legendHtml);
+})
+$(document).on('change','input[type=range]', function (name) {
+	request[name.target.name] = $(this).val();
+	updateSymbology()
+	console.log(request);
+})
+function fitBoundsPadding() {
+  // get with info div
+	const boxInfoWith = document.querySelector('.legend').offsetWidth;
+	const mapInfoWith = document.querySelector('#map').offsetWidth;
+	if (boxInfoWith >= mapInfoWith) {
+		map.fitBounds(region.getBounds(), {
+	    // https://leafletjs.com/reference-1.6.0.html#fitbounds-options-paddingtopleft
+	    'paddingBottomRight': [ 10, boxInfoWith +10]
+	  });
+	} else {
+		map.fitBounds(region.getBounds(), {
+	    // https://leafletjs.com/reference-1.6.0.html#fitbounds-options-paddingtopleft
+			'paddingBottomRight': [boxInfoWith + 10, 10]
+	  });
 
-var rangeValue = function(){
-  var newValue = elem.value;
-  var target = document.querySelector('.value');
-  target.innerHTML = newValue;
+	}
+  // sets a map view that contains the given geographical bounds
+  // with the maximum zoom level possible
+  map.fitBounds(region.getBounds(), {
+    // https://leafletjs.com/reference-1.6.0.html#fitbounds-options-paddingtopleft
+    'paddingBottomRight': [ 10, boxInfoWith +10]
+  });
 }
 
-elem.addEventListener("input", rangeValue);
+// trigger function on dom content loaded
+window.addEventListener('DOMContentLoaded', fitBoundsPadding);
 
-});
+// trigger function resize window with performant on resize
+let timeout;
+window.addEventListener('resize', () => {
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    fitBoundsPadding();
+  }, 75);
+}, false);
